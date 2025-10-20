5afcf2f8dc2a69f02beefb3b4f7c82aee207a4f2
Chaitali Barhate <chaitali.barhate.cmaug25@gmail.com>
2025-10-20 21:34:01 +0530

    Variable,Operator,Modifier_Program

---
+++ FundametalOFJava/HelloWorld.java
@@ -0,0 +1,13 @@
+package FundametalOfJava;
+/*shortcut:syso+ctrl+space
+ * println:It is throws the cursor to the next line after printing the desired result
+ * print:This method keeps the cursor on the same line.
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.print("Hello Everyone!!");
+        System.out.println("WELCOME IN Anudip Foundation");
+	}
+
+}
